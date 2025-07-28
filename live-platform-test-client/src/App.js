a74fe3f035caef6dc2db43a74b4b671d94ba0b27
cumulus <luck1306@naver.com>
2025-07-28 20:56:43 +0900

    [refactor] grouping testing component

--- live-platform-test-client/src/App.js
+++ live-platform-test-client/src/App.js
@@ -2,24 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 // import Video from "./component/common/CusVideoBox.jsx";
 // import Socket from "./component/common/CusSocketClient.jsx";
-import TestWebRtc from "./component/common/TestWebRtc.jsx";
-import RootPage from "./component/page/RootPage.jsx"
+import TestWebRtc from "./component/common/testing/TestWebRtc.jsx";
+import RootPage from "./component/page/RootPage.jsx";
 import { useState } from "react";
 import ViewerPage from "./component/page/ViewerPage.jsx";
 
 function App() {
-    const [isStreamer, setIsStreamer] = useState(false);
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* <Video /> */}
-                {/* <Socket /> */}
-                <Route path="/" element={<RootPage setState={setIsStreamer} />} />
-                <Route path="/broadcast" element={<TestWebRtc isStreamer={isStreamer} />} />
-                <Route path="/viewer" element={<ViewerPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  const [isStreamer, setIsStreamer] = useState(false);
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Video /> */}
+        {/* <Socket /> */}
+        <Route path="/" element={<RootPage setState={setIsStreamer} />} />
+        <Route
+          path="/broadcast"
+          element={<TestWebRtc isStreamer={isStreamer} />}
+        />
+        <Route path="/viewer" element={<ViewerPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
