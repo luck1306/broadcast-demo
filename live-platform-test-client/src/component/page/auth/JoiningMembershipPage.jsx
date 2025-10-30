ed0e133f6af2fbea8700ed06c9307d02e796be59
cumulus <luck1306@naver.com>
2025-10-30 23:48:18 +0900

    [update] auth layout

--- live-platform-test-client/src/component/page/auth/JoiningMembershipPage.jsx
+++ live-platform-test-client/src/component/page/auth/JoiningMembershipPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import JoinMembership from "../../../library/api/JoinMemebership";
 import { useNavigate } from "react-router-dom";
+import "../../../asset/css/LoginPage.css";
 
 const JoiningMembership = () => {
     const navigate = useNavigate;
@@ -10,52 +11,81 @@ const JoiningMembership = () => {
         password: "",
     });
     return (
-        <div>
-            <p>회원가입 페이지</p>
-            <input
-                type="text"
-                placeholder="nickname"
-                value={userInfo.nickname}
-                onChange={(e) =>
-                    setUserInfo({ ...userInfo, nickname: e.target.value })
-                }
-            />
-            <br />
-            <input
-                type="text"
-                placeholder="account_id"
-                value={userInfo.accountId}
-                onChange={(e) =>
-                    setUserInfo({ ...userInfo, accountId: e.target.value })
-                }
-            />
-            <br />
-            <input
-                type="password"
-                placeholder="password"
-                value={userInfo.password}
-                onChange={(e) =>
-                    setUserInfo({ ...userInfo, password: e.target.value })
-                }
-            />
-            <br />
-            <button
-                onClick={() => {
-                    JoinMembership(userInfo)
-                        .then((res) => {
-                            // console.log(res.data);
-                            alert("회원가입 성공");
-                            navigate("/login");
-                        })
-                        .catch((err) => {
-                            alert(
-                                `[${err.response?.data.statCode}] - ${err.response?.data.message}`
-                            );
-                        });
-                }}
-            >
-                회원가입
-            </button>
+        <div className="login-page-container">
+            <div>
+                <div className="login-page-content-container">
+                    <div className="login-page-content">
+                        <p>
+                            Join the broadcast now. <br />
+                            Moments of laughter, fun, and connection with your
+                            viewers await you.
+                        </p>
+                    </div>
+                    <div className="login-input-box-container">
+                        <h2>Signup Page</h2>
+                        <p>지금 회원가입하여 수백명의 방송인을 만나보세요!</p>
+
+                        <div className="login-input-container">
+                            <input
+                                className="login-input"
+                                type="text"
+                                placeholder="nickname"
+                                value={userInfo.nickname}
+                                onChange={(e) =>
+                                    setUserInfo({
+                                        ...userInfo,
+                                        nickname: e.target.value,
+                                    })
+                                }
+                            />
+                            <input
+                                className="login-input"
+                                type="text"
+                                placeholder="account_id"
+                                value={userInfo.accountId}
+                                onChange={(e) =>
+                                    setUserInfo({
+                                        ...userInfo,
+                                        accountId: e.target.value,
+                                    })
+                                }
+                            />
+                            <input
+                                className="login-input"
+                                type="password"
+                                placeholder="password"
+                                value={userInfo.password}
+                                onChange={(e) =>
+                                    setUserInfo({
+                                        ...userInfo,
+                                        password: e.target.value,
+                                    })
+                                }
+                            />
+                            <div className="login-button">
+                                <button
+                                    onClick={() => {
+                                        JoinMembership(userInfo)
+                                            .then((res) => {
+                                                // console.log(res.data);
+                                                alert("회원가입 성공");
+                                                navigate("/login");
+                                            })
+                                            .catch((err) => {
+                                                alert(
+                                                    `[${err.response?.data.statCode}] - ${err.response?.data.message}`
+                                                );
+                                            });
+                                    }}
+                                >
+                                    회원가입
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="login-page-red-box" />
         </div>
     );
 };
--- live-platform-test-client/src/component/page/auth/LoginPage.jsx
+++ live-platform-test-client/src/component/page/auth/LoginPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import LoginApi from "../../../library/api/LoginApi";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import "../../../asset/css/LoginPage.css";
+import { Space } from "antd";
 
 const LoginPage = () => {
     const [userInfo, setUserInfo] = useState({
@@ -19,61 +21,92 @@ const LoginPage = () => {
     };
 
     return (
-        <div>
-            <p>로그인 페이지</p>
-            <input
-                type="text"
-                placeholder="account_id"
-                value={userInfo.accountId}
-                onChange={(e) =>
-                    setUserInfo({ ...userInfo, accountId: e.target.value })
-                }
-            />
-            <br />
-            <input
-                type="password"
-                placeholder="password"
-                value={userInfo.password}
-                onChange={(e) =>
-                    setUserInfo({ ...userInfo, password: e.target.value })
-                }
-            />
-            <br />
-            <button
-                onClick={async () => {
-                    // console.log(userInfo);
-                    LoginApi(userInfo)
-                        .then((res) => {
-                            // console.log(res.data);
-                            Cookies.set(
-                                "accessToken",
-                                "Bearer " + res.data.accessToken
-                            );
-                            Cookies.set(
-                                "refreshToken",
-                                "Bearer " + res.data.refreshToken
-                            );
-                            Cookies.set(
-                                "nickname",
-                                parseJwt(res.data.accessToken)["sub"] // get logined user nickname
-                            );
-                            alert("로그인 성공");
-                            navigate("/", { replace: true });
-                        })
-                        .catch((err) => {
-                            alert(
-                                `[${err.response?.data.statCode}] - ${err.response?.data.message}`
-                            );
-                        });
-                }}
-            >
-                로그인
-            </button>
-            <br />
-            <button>
-                <Link to="/join">회원가입</Link>
-            </button>
-            <br />
+        <div className="login-page-container">
+            <div>
+                <div className="login-page-content-container">
+                    <div className="login-page-content">
+                        <p>
+                            Join the broadcast now. <br />
+                            Moments of laughter, fun, and connection with your
+                            viewers await you.
+                        </p>
+                    </div>
+                    <div className="login-input-box-container">
+                        <h2>Login Page</h2>
+                        <p>지금 로그인하여 수백명의 방송인을 만나보세요!</p>
+
+                        <div className="login-input-container">
+                            <input
+                                className="login-input"
+                                type="text"
+                                placeholder="account_id"
+                                value={userInfo.accountId}
+                                onChange={(e) =>
+                                    setUserInfo({
+                                        ...userInfo,
+                                        accountId: e.target.value,
+                                    })
+                                }
+                            />
+                            <input
+                                className="login-input"
+                                type="password"
+                                placeholder="password"
+                                value={userInfo.password}
+                                onChange={(e) =>
+                                    setUserInfo({
+                                        ...userInfo,
+                                        password: e.target.value,
+                                    })
+                                }
+                            />
+                            <div className="login-button">
+                                <button
+                                    onClick={async () => {
+                                        // console.log(userInfo);
+                                        LoginApi(userInfo)
+                                            .then((res) => {
+                                                // console.log(res.data);
+                                                Cookies.set(
+                                                    "accessToken",
+                                                    "Bearer " +
+                                                        res.data.accessToken
+                                                );
+                                                Cookies.set(
+                                                    "refreshToken",
+                                                    "Bearer " +
+                                                        res.data.refreshToken
+                                                );
+                                                Cookies.set(
+                                                    "nickname",
+                                                    parseJwt(
+                                                        res.data.accessToken
+                                                    )["sub"] // get logined user nickname
+                                                );
+                                                alert("로그인 성공");
+                                                navigate("/", {
+                                                    replace: true,
+                                                });
+                                            })
+                                            .catch((err) => {
+                                                alert(
+                                                    `[${err.response?.data.statCode}] - ${err.response?.data.message}`
+                                                );
+                                            });
+                                    }}
+                                >
+                                    로그인
+                                </button>
+                            </div>
+                            <p className="signin-text">
+                                아직 회원이 아니신가요? <Space />
+                                <Link to="/join">회원가입</Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="login-page-red-box" />
         </div>
     );
 };
